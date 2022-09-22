d4641aa936962f725641c819dffc295bed54a1ee
Jay Miller <kjaymiller@gmail.com>
2022-09-22 11:33:07 -0400

    update function init

--- TimerTrigger/__init__.py
+++ TimerTrigger/__init__.py
@@ -5,15 +5,17 @@ from image import overlay_text
 import azure.functions as func
 from twitter import send_tweet
 from io import BytesIO
+import os
 
+image_path = os.environ.get("IMAGE_TEMPLATE_PATH")
 
 def main(mytimer: func.TimerRequest) -> None:
     queue = load_queue("31daysofndqueue")
-    queue.send_message("2 - This is a numbered queue message")
-    msg = queue.receive_messages().next()
+    msg = queue.receive_messages().next() # TODO: #10 Add Logic to check queue count
+    
     # Message is in format "Index - Text"
     index_, text = msg.content.split(" - ", maxsplit=1)
-    img = overlay_text(index_, text, image_path="./assets/test_cat.jpeg")
+    img = overlay_text(index_, text, image_path=image_path) # TODO: #9 Change to path stored in environment variable
     image = BytesIO()
     img.save(image, format="JPEG")
     image.seek(0)
