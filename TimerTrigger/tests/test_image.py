93070d59ea789382265a7214899a690c6052add8
Jay Miller <kjaymiller@gmail.com>
2022-09-22 11:33:56 -0400

    add tests

---
+++ TimerTrigger/tests/test_image.py
@@ -0,0 +1,7 @@
+import pytest
+import pathlib
+
+def test_assets_exist():
+    """error if the necessary files are not present in the assets folder"""
+    assert pathlib.Path('./assets/Adhd Awareness Month-6.png').exists()
+    assert pathlib.Path('./assets/Lato-BoldItalic.ttf').exists()
