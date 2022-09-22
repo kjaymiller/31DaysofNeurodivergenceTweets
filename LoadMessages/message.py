12e03278fb66f4b3c161e27fd88bfecc2a9ae724
Jay Miller <kjaymiller@gmail.com>
2022-09-22 11:32:54 -0400

    remove insert function

--- LoadMessages/message.py
+++ LoadMessages/message.py
@@ -1,13 +1,18 @@
 import typing
-from storage import load_queue
+from azure.storage.queue import QueueClient
 
 
-def load_messages(messages: typing.Sequence[str], queue_name: QueueClient, format: typing.Optional[function]):
-    """Iterate through a queue and add the formatted messages"""
-    
-    for index_, message in enumerate(messages, start=1):
-        azaueue.send_message(f"{index_} - {message}", time_to_live=-1)
+def load_messages(
+    messages: typing.Sequence[str],
+    queue: QueueClient,
+    # pre-upload: typing.Optional[list[function]] = None, # TODO: #11 Add Formatting Rules
+    **kwargs,
+):
+    """
+    Iterate through a `messages` optionally applying a formatted messages.
 
-    return azqueue
-
-def split_text()
+    """
+    # #11 Will change this to just messages and then you'll have to pass in formatting functions to `pre_upload` the message
+    for index_, message in enumerate(messages, start=1,): 
+        # #11 will check for pre_upload
+        yield queue.send_message(f"{index_} - {message}", **kwargs)
