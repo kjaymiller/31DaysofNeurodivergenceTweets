81611a24f2f210e295d855085e4450aaf6d0be5b
Jay <kjaymiller@gmail.com>
2022-10-22 15:05:08 -0700

    adds logic to messages.py

--- LoadMessages/message.py
+++ LoadMessages/message.py
@@ -1,18 +1,88 @@
+import os
 import typing
+import typer
+import pathlib
 from azure.storage.queue import QueueClient
+from azure.core.exceptions import ResourceExistsError
 
 
+app = typer.Typer(rich_help_panel="rich")
+
+@app.command()
 def load_messages(
-    messages: typing.Sequence[str],
-    queue: QueueClient,
-    # pre-upload: typing.Optional[list[function]] = None, # TODO: #11 Add Formatting Rules
-    **kwargs,
+    connection_string: str = typer.Option(
+            None, "--conn", envvar="AZURE_STORAGE_CONNECTION_STRING",
+            help="Connection string for the Azure storage account",
+            rich_help_panel="Azure Connection Options"
+    ),
+    queue_name: str = typer.Option(
+        None, "--queue", envvar="AZURE_STORAGE_QUEUE_NAME",
+        help="Name of the Azure storage queue",
+        rich_help_panel="Azure Connection Options"
+    ),
+    filepath: typing.Optional[pathlib.Path] = typer.Option(
+        None, "-f", "--filepath",
+        help="Path to the file containing the messages to load",
+        rich_help_panel="Message Options"
+    ),
+    messages: typing.Optional[list[str]] = typer.Option(
+        None, "-m", "--message",
+        help="Messages to load",
+        rich_help_panel="Message Options"
+    ),
+    start: typing.Optional[int] = typer.Option(1,
+        help="Start index for the messages",
+        rich_help_panel="Message Options",
+    ),
 ):
     """
     Iterate through a `messages` optionally applying a formatted messages.
-
+    
+    Messages are loaded with the format:
+    
+    ```
+    {INDEX STARTING WITH `start`}: {MESSAGE}
+    ```
     """
-    # #11 Will change this to just messages and then you'll have to pass in formatting functions to `pre_upload` the message
-    for index_, message in enumerate(messages, start=1,): 
+    queue = QueueClient.from_connection_string(connection_string, queue_name)
+    
+    try:
+        queue.create_queue()
+    
+    except ResourceExistsError:
+        pass
+    
+    # if queue doesn't exist, create it
+
+    if filepath:
+        with open(filepath, "r") as f:
+            messages = f.readlines()
+    
+    for index_, message in enumerate(messages, start=start): 
         # #11 will check for pre_upload
-        yield queue.send_message(f"{index_} - {message}", **kwargs)
+        queue.send_message(f"{index_} - {message}", time_to_live=60*60*24*1)
+
+        typer.echo(f"Sent {len(messages)} messages")
+
+@app.command()   
+def clear_messages(
+    connection_string: str = typer.Option(
+            None, "--conn", envvar="AZURE_STORAGE_CONNECTION_STRING",
+            help="Connection string for the Azure storage account",
+            rich_help_panel="Azure Connection Options"
+    ),
+    queue_name: str = typer.Option(
+        None, "--queue", envvar="AZURE_STORAGE_QUEUE_NAME",
+        help="Name of the Azure storage queue",
+        rich_help_panel="Azure Connection Options"
+    ),
+):
+    """
+    Clear all messages from a queue
+    """
+    queue = QueueClient.from_connection_string(connection_string, queue_name)
+    queue.clear_messages()
+    typer.echo(f"All messages from \"{queue_name}\" cleared")
+
+if __name__ == "__main__":
+    app()
